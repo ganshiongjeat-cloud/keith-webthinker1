9fdefe21cc64c2ff0ad292774153b6030f90d1fe
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-01-18 09:20:52 +0000

    keith

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -2,6 +2,71 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
-
+# print("red")
+# print("orange")
+# print("yellow")
+# print("green")
+# print("blue")
+# print("indigo")
+# print("violet")
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+
+
+#repeat 10 times
+#  say hey
+#  move 10 steps
+ 
+#ask user what is your age and wait
+# if answer is below 18
+#print access denied
+#otherwise
+# print welcome 
+
+#=========task 1========== 
+#pick up an item
+# check material of item
+#=========task 2==========
+#if material is glass
+#put in glass recycling bin
+#=========task 3==========
+#if material is plastic
+#put in plastic recycling bin
+#=========task 4==========
+# if material is paper 
+# put in paper recycling bin 
+
+
+#create variable for glass ,plastic and paper
+#create a repeat loop until there is no more items 
+#if item is glass 
+  # put in glass bin
+# else if item is plastic 
+  # put item in plastic bin 
+# else if item is paper
+
+
+#ask user to enter a secret phrase 
+# if phrase is wrong 
+  # send the user a error message
+# if phrase is correct
+  # send them a congragulations message 
+
+
+#create a variable for 1%
+#every 5 marks a student gets for test 1
+  # add 1 to the variable
+# every 2.5 marks a student gets for test 2
+  # add 1 to the variable 
+# every 2.5 marks a student gets for test 3
+  # add 1 to the variable 
+  # add up all the adds to the variable and total up all of them
+    # write totaled variable out of 100
+
+
+#create variable test1 test2 test3
+#test1 = test1 score * 20%
+#test2 = test2 score * 40%
+#test3 = test3 score * 40%
+#create finalscore variable
+#finalscore = test1 + test2 + test3
