877014d9b23700b4143e7a6133c2d615b3ab7c47
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-02-01 09:34:00 +0000

    keith

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,117 @@
-print("Hello from lesson 3")
+import math
+
+
+
+totalscore = 0
+Ascore = 60
+Bscore = 58
+Cscore = 98
+Dscore = 79
+totalscore = Ascore + Bscore + Cscore + Dscore
+averagescore = totalscore * 4
+
+sentence = "minecraft real??!!"
+print(sentence) #print variable that contains the string my sentence
+
+age = 14
+print(age)
+
+x=10
+x=20
+print(x)
+
+variable1 = 3
+variable2 = 5
+print (variable1 + variable2)
+total = variable1 + variable2
+print (total)
+print (variable1 * variable2)
+print (variable1 / variable2)
+print (variable1 - variable2)
+
+task3a = 42.56789
+print (round(task3a,2))
+
+
+a = 1
+b = 2
+ans = a + b
+print (ans)
+
+
+c = 50
+d = 10
+e = 3
+ans = (c - d) * e
+print (ans)
+
+
+f = 100
+ans = f / 4
+print (ans)
+
+
+ans = 40
+print (ans)
+ 
+
+a = 15
+b = 58
+ans = (15 * 2) + 58
+print (ans)
+
+ 
+a = 96
+b = 18 
+c = 2
+bbb = b * 3 
+cccccc = c * 6
+total = a + bbb + cccccc
+total1 = total * 1.1
+ans = total1 - 25
+print (ans)
+
+seafood = 100
+sideD = 20 
+rice = 2.5
+total = ((seafood * 5) + (sideD * 8) + (rice * 20))
+sctotal = total * 1.1
+gsttotal = sctotal * 1.09
+print(round(gsttotal,2))
+
+
+#abs(-5) = 5
+#abs(5) = 5
+#sq = math.pow(2,2)
+#sqrt = math.sqrt(4)
+
+numbatches = 9.8/1
+print (math.floor(numbatches))
+
+numbags = 27/4
+print(math.ceil(numbags))
+
+
+
+# #what we learn
+# variables
+# datatypes
+# 1) integers - whole numbers
+# 2)float - decimal number
+# 3) string - char
+
+# pedmas order of operations
+# p arenthesis
+# e xponent
+# d ivide
+# a dd
+# s ubtract
+
+# math fuctions 
+# round - round to the nearest smth 
+# abs = turn negative into positive pos into pos
+
+# import math library
+# math.floor return largest integer  ,smaller or equal, to x 
+# math.ceil return smallest integer that is greater or equal to x
+#math.pi gives u pi
--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,24 @@
 print("Hello from lesson 4")
+
+
+yourname = input("what is your name")
+print ("My name is "+yourname)
+
+favcolour = input("what is your fav colour")
+print (favcolour)
+
+userage = input("what is your age")
+finalage = int(userage) + 1
+str(finalage)
+print("next year you will be" + finalage + "years old")
+favhob = input("what is your favourite hobby")
+print ("I enjoy" + favhob)
+
+numb = input("pick a random number")
+finalnumb = int(numb) * 2
+str(finalnumb)
+print("double your number is" + finalnumb)
+
+birthyear = input("what year were you born")
+age = 2026 - int(birthyear)
+print("you are" + age "years old")
