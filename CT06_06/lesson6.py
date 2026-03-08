c1ed397bf9961705ffb1f8e725080ec6c0eef814
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-03-08 08:24:34 +0000

    q1

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,54 @@
 print("Hello from lesson 6")
+
+numofstudents = input("enter the total number of students")
+totalmark = 0
+for i in range(int(numofstudents)):
+    studentmarks = input("whats your mark")
+    totalmark = totalmark + int(studentmarks)
+average = int(totalmark) / int(numofstudents)
+print(average)
+
+
+
+
+for i in range(3):
+    print("hello, world")
+
+for i in range(5):
+    print(i)
+
+print("hello, world")
+
+ifnt = 5
+
+print ("hello, world")
+
+age = input("what be your age")
+print(age)
+
+name = "alice"
+print(name)
+
+x = 5
+print(x)
+
+print("hello, world")
+
+age = 25
+print(int(age) + 1)
+
+print("repeat" * 3)
+
+x = (10)
+y = (x / 2)
+
+number = 10
+print(int(number) - 5)
+
+year = 2023
+
+print("the year is "str(year))
+
+end = 5
+for i in range(end):
+    print(i)
--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,48 @@
 print("Hello from lesson 7")
 
+for i in range(1 , 11):
+    print (i)
+
+for i in range(2 , 21 , 2):
+    print(i)
+
+for i in range(11 , 0 , -1):
+    print(i)
+
+words = input("what do u want to repeat")
+times = input("number of times")
+for i in range(int(times)):
+    print(words)
+
+name = input("what is ur name")
+num = input("how many times you want repeat")
+for i in range(int(num)):
+    print("nice to meet you " + name)
+
+sum = 0
+for i in range(5):
+    ask = input("what is number#" + str(i))
+    sum += int(ask)
+print(sum)
+
+userb = input("what number's timetable u wan")
+for i in range(1 , 13):
+    #print(int(ask) * int(i))
+    print(str(i) + "x" + str(userb) + "=" + str(i*int(userb)))
+
+sum = 0
+for i in range(5):
+    ask = input("what is ur marks")
+    sum += int(ask)
+average = sum/5
+print(average)
+
+
+
+
+
+
+
+
+
+
