bde4011ad8e8fd6bb4844069fa1f91c3b9b2f4a9
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-03-08 08:24:15 +0000

    q1

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,67 @@
 print("Hello from lesson 5")
+
+
+#name = input("what is your name")
+#message = input("what personal message would you like to send")
+#agethisyear = input("what is your age this year")
+#print ("happy " + agethisyear + "th birthday " + name + message)
+
+#for i in range(10):
+#    print("MinECrAft")
+
+#for i in range(100):
+#    print("i like ramen")
+#    print("GIVE ME MORE!!!")
+
+for i in range(0,60):
+    print(i)
+
+for i in range(1,6):
+    print(i)
+
+for i in range(51,101):
+    print(i)
+
+for i in range(18, 30):
+    print(i)
+
+
+for i in range(2, 25, 2):
+    print(i)
+
+for i in range(8,96, 8):
+    print(i)
+
+for i in range(5,0,-1):
+    print(i)
+
+for i in range(100,0,-2):
+    print(i)
+
+for i in range(0,100,3):
+    print(i)
+
+name = input("what be your name ")
+
+for i in name:
+    print("give me a " + i)
+
+print("ready")
+for i in range(3,0,-1):
+    print(i)
+
+for i in range(10,0,-1):
+    print(i)
+if (i==1):
+    print("BOO")
+
+num = 0
+for i in range(10):
+    # num = num +1
+    num = num +i
+    print(num)
+
+start = input("write a random nmber")
+stop = input("write a different random no.")
+for i in range(int(start), int(stop) + 1):
+    print(i)
