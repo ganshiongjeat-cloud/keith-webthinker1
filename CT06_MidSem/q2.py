a4516ab5cc7b0bb2403b6438e06089cebc7a90e1
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-03-08 08:45:37 +0000

    q2

---
+++ CT06_MidSem/q2.py
@@ -0,0 +1,69 @@
+"""
+============================================================
+Q2. Savings Simulator
+============================================================
+You are building a small savings simulator.
+A person starts with a certain amount of money.
+
+Every day, the person saves more money than the previous day:
+
+Day 1 → save $1
+Day 2 → save $2
+Day 3 → save $3
+Day 4 → save $4
+… and so on
+
+The program must:
+- Ask for the starting amount of money.
+- Ask for the number of days.
+- For each day, add the correct savings amount.
+- Print the total money after each day.
+- Finally, print the final total amount.
+
+Program Requirements:
+- Use a for loop.
+- Use range() correctly.
+- Update the total amount inside the loop.
+- Print exactly in this format:
+    Day <X>: $<Y>
+- After the loop, print:
+    Total amount saved = $<Z>
+
+Note:
+- Follow the input order exactly as shown in the Test Cases.
+- You must get the correct output for ALL 3 test cases.
+
+============================================================
+"""
+
+# ============================================================
+# Step 1: Ask for Starting Amount
+# ============================================================
+start =input("how much is currently in your bank account ")
+
+
+# ============================================================
+# Step 2: Ask for Number of Days
+# ============================================================
+day = input("how many days have you been saving")
+day = int(day)
+
+# ============================================================
+# Step 3: Use a for loop to simulate savings
+# ============================================================
+# - Use range() correctly
+# - Add the correct daily savings amount
+# - Update and print the total each day
+#   Day <X>: $<Y>
+# ============================================================
+for i in range (1 , day + 1):
+    dailysaving = (i)
+    start = (int(start) + int(dailysaving))
+    print ( "your total now is $" + str(start) + " for day " + str(i))
+# ============================================================
+# Step 4: Print Final Total
+# ============================================================
+# - Print the final amount in this format:
+#   Total amount saved = $<Z>
+# ============================================================
+print("your total amount saved is $" + str(start))
